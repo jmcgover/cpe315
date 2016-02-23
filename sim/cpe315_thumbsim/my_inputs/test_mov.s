20027cb58e7addbfea0bc8872f86b5efae25a39c
Jeff McGovern <jmcgover@calpoly.edu>
2016-02-23 21:36:59 +0000

    adjsuted the sim header

--- sim/cpe315_thumbsim/my_inputs/test_mov.s
+++ sim/cpe315_thumbsim/my_inputs/test_mov.s
@@ -1,4 +1,13 @@
-    .global main
+        .arch armv6
+        .fpu softvfp
+        .code   16
+        .file   "fib.c"
+        .text
+        .align  2
+        .global main
+        .code   16
+        .thumb_func
+        .type   main, %function
 main:
         push {r7, lr}
         mov r0, #7
