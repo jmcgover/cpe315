2ae82ff1998709169e6922728d6adf07f5592626
Jeff McGovern <jmcgover@calpoly.edu>
2016-02-24 15:28:06 +0000

    made the test_cmp sim file

--- sim/cpe315_thumbsim/my_inputs/test_cmp.s
+++ sim/cpe315_thumbsim/my_inputs/test_cmp.s
@@ -10,6 +10,14 @@
         .type   main, %function
 main:
         push {r7, lr}
+        mov r1, #1
+        mov r2, #2
+        cmp r1, r2
+        blt answer
+        mov r0, #2
+        b return
+answer:
         mov r0, #7
+return:
         pop {r7, pc}
 
